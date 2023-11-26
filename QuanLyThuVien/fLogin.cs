042fe4f0b6f46ea8d087664e92d5928516ac24cd
21522631 <106432240+21522631@users.noreply.github.com>
2023-11-26 21:50:58 +0700

    Update fLogin.cs
    chỉnh một chút bên form login

--- QuanLyThuVien/fLogin.cs
+++ QuanLyThuVien/fLogin.cs
@@ -17,26 +17,6 @@ namespace QuanLyThuVien
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-          
-        }
-
         private void btnLogin_Click(object sender, EventArgs e)
         {
             fAdminManager f = new fAdminManager();
