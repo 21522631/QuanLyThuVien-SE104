5beb1deb3dbff89e7439381ae9b3f0350f983dce
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-05 14:54:00 +0700

    update dal nguoi dung

--- DAL/NguoiDungDAL.cs
+++ DAL/NguoiDungDAL.cs
@@ -5,16 +5,23 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
-
+using DTO;
 namespace DAL
 {
     public class NguoiDung : DataProvider
     {
-        DataProvider  data = new DataProvider();
+        //DataProvider  data = new DataProvider();
         public DataTable GetAllNguoiDung()
         {
             string query = "SELECT * FROM NGUOIDUNG";
             return base.ExecuteQuery(query);
         }
+        public DataTable GetNguoiDungByTaiKhoanMatKhau(string TenDangNhap, string MatKhau)
+        {
+            string query = "SELECT * FROM NGUOIDUNG" +
+                "WHERE TENDANGNHAP = '" + TenDangNhap + "' AND MATKHAU = '" + MatKhau + "'";
+            return base.ExecuteQuery(query);
+        }
     }
+
 }
