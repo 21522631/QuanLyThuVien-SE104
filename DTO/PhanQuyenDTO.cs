76da6eb011ae5fa6be117d0f7317640f7e7b868d
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-05 13:31:36 +0700

    thêm DTO phân quyền

---
+++ DTO/PhanQuyenDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PhanQuyen
+    {
+        public int IDNhomNguoiDung {  get; set; }
+        public int IDChucNang { get; set; }
+    }
+}
