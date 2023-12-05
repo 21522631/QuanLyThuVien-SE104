4417637d0dadcff45981b4f86edeb8d776b5024e
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-05 14:31:54 +0700

    thêm Dal người dùng

---
+++ DAL/NguoiDungDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class NguoiDung : DataProvider
+    {
+        DataProvider  data = new DataProvider();
+        public DataTable GetAllNguoiDung()
+        {
+            string query = "SELECT * FROM NGUOIDUNG";
+            return base.ExecuteQuery(query);
+        }
+    }
+}
