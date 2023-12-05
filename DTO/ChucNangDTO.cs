2c4c22a84a06c0fdbe8167875aa2992b390eb99d
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-05 13:29:39 +0700

    Thêm DTO Chức Năng

---
+++ DTO/ChucNangDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ChucNang
+    {
+        public int ID { get; set; }
+        public string MaChucNang { get; set; }
+        public string TenChucNang { get; set; } 
+        public string TenManHinhDuocLoad { get; set; }
+    }
+}
