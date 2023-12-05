f54d448ee089083298798a20cd781c8e061a4cfd
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-05 13:23:01 +0700

    Thêm DTO thể loại

---
+++ DTO/TheLoaiDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TheLoaiDTO
+    {
+        public int ID { get; set; }
+        public string MaTheLoai {  get; set; }
+        public string TenTheLoai { get; set; }
+    }
+}
