b58aeac1dfd034818f6d4c4bf875140c8f2b7c8a
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:39:30 +0700

    Thêm DTO loại độc giả

---
+++ DTO/LoaiDocGiaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LoaiDocGia
+    {
+        public int ID { get; set; }
+        public string MaLoaiDocGia { get; set; }
+        public string TenLoaiDocGia { get; set; }
+    }
+}
