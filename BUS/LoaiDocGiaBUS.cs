39925271373456b907828f3a077608171e3d3ec9
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 11:48:08 +0700

    Thêm bus Loại độc giả và chỉnh một số control

---
+++ BUS/LoaiDocGiaBUS.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class LoaiDocGiaBUS
+    {
+        private static LoaiDocGiaBUS instance;
+        private LoaiDocGiaBUS() { }
+        public static LoaiDocGiaBUS Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new LoaiDocGiaBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllLoaiDocGia()
+        {
+            return LoaiDocGiaDAL.Instance.GetAllLoaiDocGia();
+        }
+    }
+}
--- GUI/fQuanLyDocGia.cs
+++ GUI/fQuanLyDocGia.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO;
+using BUS;
 
 namespace GUI
 {
@@ -16,50 +18,19 @@ namespace GUI
         {
             InitializeComponent();
         }
-
-        private void label4_Click(object sender, EventArgs e)
+        private void fQuanLyDocGia_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void tabPage3_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void dgvLoaiDocGia_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        private void fQuanLyDocGia_Load(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
