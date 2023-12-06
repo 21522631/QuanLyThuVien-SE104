2294cc17a1d515a528b2057159d2e1c96a42b5f9
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:27:36 +0700

    Thêm DTO CT_Tác Giả ,Sách,

---
+++ DTO/CT_TacGia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CT_TacGia
+    {
+        public int IDSach { get; set; }
+        public int IDTacGia { get; set; }
+    }
+}
---
+++ DTO/Sach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Sach
+    {
+        public int ID { get; set; }
+        public string MaSach {  get; set; }
+        public string TenSach { get; set; }
+        public string IDTheLoai { get; set; }
+        public string NhaXuatBan { get; set; }
+        public int NamXuatBan { get; set; }
+        public int SoLuong {  get; set; }
+        public int GiaTien { get; set; }
+    }
+}
