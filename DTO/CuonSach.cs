4280f025396bb6a3b6ea4e569fe78fe741f181d1
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:29:17 +0700

    Thêm DTO Cuốn Sách

---
+++ DTO/CuonSach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CuonSach
+    {
+        public int ID { get; set; }
+        public string MaCuonSach { get; set; }
+        public int IDSach { get; set; }
+        public int TinhTrang { get; set; }
+    }
+}
