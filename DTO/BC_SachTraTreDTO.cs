03117e23264bdcc97c02c46dd9bcf61fd32eb4b8
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 10:08:57 +0700

    Thêm DTO BC Sách Trả Trễ

---
+++ DTO/BC_SachTraTreDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BC_SachTraTre
+    {
+        public DateTime Ngay { get; set; }
+        public int IDCuonSach { get; set; }
+        public DateTime NgayMuon { get; set; }
+        public int SoNgayTraTre { get; set; }
+    }
+}
