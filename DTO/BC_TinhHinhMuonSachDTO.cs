e38e7a4787310e660f3815211f5934eb966a2a8a
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:55:58 +0700

    Thêm DTO BC tình hình mượn sách

---
+++ DTO/BC_TinhHinhMuonSachDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BC_TinhHinhMuonSach
+    {
+        public int ID { get; set; }
+        public string MaBCTHMS { get; set; }
+        public int Thang { get; set; }
+        public int Nam { get; set; }
+        public int TongSoLuotMuon { get; set; }
+    }
+}
