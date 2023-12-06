f4bfce38539d098db1753e83c4747fae16b61d37
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:53:25 +0700

    Thêm DTO Phiếu Thu Tiền Phạt

---
+++ DTO/PhieuThuTienPhatDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PhieuThuTienPhat
+    {
+        public int ID { get; set; }
+        public string SoPTTP { get; set; }
+        public int IDDocGia { get; set; }
+        public DateTime NgayThu { get; set; }
+        public int TongNo { get; set; }
+        public int SoTienThu { get; set; }
+        public int ConLai { get; set; }
+    }
+}
