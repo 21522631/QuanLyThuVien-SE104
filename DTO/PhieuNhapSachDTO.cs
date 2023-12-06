5b244aadf27e613e8279867ecb903634c160e981
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:33:07 +0700

    Thêm DTO phiếu nhập sách và chỉnh sửa các DTO khác

---
+++ DTO/PhieuNhapSachDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PhieuNhapSach
+    {
+        public int ID {  get; set; }
+        public string SoPNS { get; set; }
+        public DateTime NgayLap { get; set; }
+        public int TongTien { get; set; }
+    }
+}
