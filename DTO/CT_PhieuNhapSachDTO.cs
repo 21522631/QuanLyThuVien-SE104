d0cef6fbb82d751d4efd4522f2902f60de4b910b
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:37:19 +0700

    Thêm DTO CT_Phiếu nhập sách

---
+++ DTO/CT_PhieuNhapSachDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CT_PhieuNhapSach
+    {
+        public int IDPNS { get; set; }
+        public int IDSach { get; set; }
+        public int SoLuongSach { get; set; }
+        public int DonGia {  get; set; }
+        public int ThanhTien { get; set; }
+    }
+}
