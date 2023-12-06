5c0806bafcf01682ec3402e5960623460663670e
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 13:34:51 +0700

    Thêm DAL độc giả

---
+++ DAL/DocGiaDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class DocGiaDAL
+    {
+        private static DocGiaDAL instance;
+        private DocGiaDAL() { }
+        public static DocGiaDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new DocGiaDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllDocGia()
+        {
+            string query = "SELECT * FROM DOCGIA";
+            return DataProvider.Instance.ExecuteQuery(query);
+        }
+    }
+}
