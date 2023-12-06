f1b395a402074a4e69a1623f2996bf7147a81514
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 10:04:08 +0700

    Thêm DTO CT BC tình hình mượn sách theo thể loại

---
+++ DTO/CT_BC_TinhHinhMuonSachDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CT_BC_TinhHinhMuonSach
+    {
+        public int IDBCTHMS { get; set; }
+        public int IDTheLoai { get; set; }
+        public int SoLuotMuon { get; set; }
+        public float TiLe { get; set; }
+    }
+}
