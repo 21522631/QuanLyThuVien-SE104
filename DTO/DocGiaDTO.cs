ef20904365962382cfd0111fffb0092225ac6eba
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:44:40 +0700

    Thêm DTO độc giả

---
+++ DTO/DocGiaDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DocGia
+    {
+        public int ID {  get; set; }
+        public string MaDocGia {  get; set; }
+        public int IDLoaiDocGia { get; set; }
+        public string TenDocGia { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string DiaChi {  get; set; }
+        public string Email { get; set; }
+        public DateTime NgayLapThe {  get; set; }
+        public DateTime NgayHetHan {  get; set; }
+        public int TongNo { get; set; }
+        public int IDNguoiDung { get; set; }
+
+    }
+}
