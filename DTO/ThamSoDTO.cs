8691cd05f6f9c5e35d6e731f34388dd224d6a716
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 10:14:02 +0700

    Thêm DTO tham số

---
+++ DTO/ThamSoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ThamSo
+    {
+        public int ID { get; set; }
+        public int TuoiToiThieu { get; set; }
+        public int TuoiToiDa { get; set; }
+        public int ThoiHanThe { get; set; }
+        public int KhoangCachNamXB { get; set; }
+        public int SoNgayMuonToiDa { get; set; }
+        public int SoSachMuonToiDa { get; set; }
+        public int DonGiaPhat { get; set; }
+        public int ApDungQDTienThu { get; set; }
+    }
+}
