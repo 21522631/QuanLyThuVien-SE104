a7fe37cebbad743e01c77c41acf2020cd53f7f11
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 22:03:56 +0700

    Thêm DAL tham số

---
+++ DAL/ThamSoDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class ThamSoDAL
+    {
+        private static ThamSoDAL instance;
+        private ThamSoDAL() { }
+        public static ThamSoDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new ThamSoDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public ThamSo GetThamSo()
+        {
+            ThamSo thamso = new ThamSo();
+            DataTable data = new DataTable();
+            string query = "SELECT * FROM THAMSO";
+            data = DataProvider.Instance.ExecuteQuery(query);
+            thamso.ID = Convert.ToInt32(data.Rows[0]["ID"].ToString());
+            thamso.TuoiToiThieu = Convert.ToInt32(data.Rows[0]["TUOITOITHIEU"].ToString());
+            thamso.TuoiToiDa = Convert.ToInt32(data.Rows[0]["TUOITOIDA"].ToString());
+            thamso.ThoiHanThe = Convert.ToInt32(data.Rows[0]["THOIHANTHE"].ToString());
+            thamso.KhoangCachNamXB = Convert.ToInt32(data.Rows[0]["KHOANGCACHNAMXB"].ToString());
+            thamso.SoNgayMuonToiDa = Convert.ToInt32(data.Rows[0]["SONGAYMUONTOIDA"].ToString());
+            thamso.SoSachMuonToiDa = Convert.ToInt32(data.Rows[0]["SOSACHMUONTOIDA"].ToString());
+            thamso.DonGiaPhat = Convert.ToInt32(data.Rows[0]["DONGIAPHAT"].ToString());
+            thamso.ApDungQDTienThu = Convert.ToInt32(data.Rows[0]["APDUNGQDTIENTHU"].ToString());
+            return thamso;
+        }
+    }
+}
