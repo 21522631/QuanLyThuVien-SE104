4b22f950c59d262ad59da122bf0290569f224871
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-06 09:48:26 +0700

    Thêm DTO phiếu mượn trả

---
+++ DTO/PhieuMuonTraDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PhieuMuonTra
+    {
+        public int ID { get; set; }
+        public string SoPMT { get; set; }
+        public int IDDocGia { get; set; }
+        public int IDCuonSach { get; set; }
+        public DateTime NgayMuon { get; set; }
+        public DateTime NgayPhaiTra { get; set; }
+        public DateTime NgayTra { get; set; }
+        public int TienPhat { get; set; }
+    }
+}
