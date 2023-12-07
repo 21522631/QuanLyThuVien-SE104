7503b514314f6cb4c8714236b85c68c3dfe31504
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 12:43:56 +0700

    Thêm DAL thể loại

---
+++ DAL/TheLoaiDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TheLoaiDAL
+    {
+        private static TheLoaiDAL instance;
+        private TheLoaiDAL() { }
+        public static TheLoaiDAL Instance
+        {
+            get
+            {
+                if (instance == null) instance = new TheLoaiDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
