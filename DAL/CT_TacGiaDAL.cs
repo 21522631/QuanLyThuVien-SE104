aac5c0a4dc36c9f526c144879ed4a9e11e9fbb49
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 13:34:49 +0700

    Thêm DAL CT Tác giả

---
+++ DAL/CT_TacGiaDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CT_TacGiaDAL
+    {
+        private static CT_TacGiaDAL instance;
+        private CT_TacGiaDAL() { }
+        public static CT_TacGiaDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new CT_TacGiaDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
