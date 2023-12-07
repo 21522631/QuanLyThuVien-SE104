c4d4a9d0346dca7dc96ac2632a76686a4c805eb5
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 13:36:43 +0700

    Thêm BUS ct tác giả

---
+++ BUS/CT_TacGiaBUS.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class CT_TacGiaBUS
+    {
+        private static CT_TacGiaBUS instance;
+        private CT_TacGiaBUS() { }
+        public static CT_TacGiaBUS Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new CT_TacGiaBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
