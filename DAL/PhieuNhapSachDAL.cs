5b0eeaadb312e57394dd484e7d88a65373ff34d0
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 19:21:01 +0700

    Thêm DAL phiếu nhập sách

---
+++ DAL/PhieuNhapSachDAL.cs
@@ -0,0 +1,41 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PhieuNhapSachDAL
+    {
+        private static PhieuNhapSachDAL instance;
+        private PhieuNhapSachDAL() { }
+
+        public static PhieuNhapSachDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new PhieuNhapSachDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllPhieuNhapSach()
+        {
+            string query = "SELECT SOPNS, NGAYLAP, TONGTIEN FROM PHIEUNHAPSACH";
+            return DataProvider.Instance.ExecuteQuery(query);
+        }
+        public int InsertPhieuNhapSach(string NgayLap)
+        {
+            string query = "INSERT INTO PHIEUNHAPSACH VALUES('" + NgayLap + "', 0)";
+            return DataProvider.Instance.ExecuteNonQuery(query);
+        }
+        public int DeletePhieuNhapSach(string SoPNS)
+        {
+            string query = "DELETE FROM PHIEUNHAPSACH WHERE SOPNS = '" + SoPNS + "'";
+            return DataProvider.Instance.ExecuteNonQuery(query);
+        }
+    }
+}
