aff4d8fa2b97b0bafd7ddbbc0cd3995dd018d7f7
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 13:32:51 +0700

    Thêm BUS sách

---
+++ BUS/SachBUS.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class SachBUS
+    {
+        private static SachBUS instance;
+        private SachBUS() { }
+        public static SachBUS Instance
+        {
+            get
+            {
+                if (instance == null) instance = new SachBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
