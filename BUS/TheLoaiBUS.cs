ae65a807216923af980c0a550f64e44fde6b4fab
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-07 12:45:21 +0700

    Thêm bus thể loại

---
+++ BUS/TheLoaiBUS.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class TheLoaiBUS
+    {
+        private static TheLoaiBUS instance;
+        private TheLoaiBUS() { }
+
+        public static TheLoaiBUS Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new TheLoaiBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
