69023217dea61bc99a98bdff457f8731978d18f3
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-15 21:50:59 +0700

    Thêm bus bc tình hình mượn sách

---
+++ BUS/BC_TinhHinhMuonSachBUS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BC_TinhHinhMuonSachBUS
+    {
+        private static BC_TinhHinhMuonSachBUS instance;
+        private BC_TinhHinhMuonSachBUS() { }
+
+        public static BC_TinhHinhMuonSachBUS Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new BC_TinhHinhMuonSachBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+    }
+}
