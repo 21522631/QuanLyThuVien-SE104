9a072b7a7ac6af10ea34ed3cfce65e07d2b3beda
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-17 18:57:11 +0700

    Sửa BUS và DAL CT_BC Tình Hình Mượn Sách

---
+++ BUS/CT_BC_TinhHinhMuonSachBUS.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class CT_BC_TinhHinhMuonSachBUS
+    {
+        private static CT_BC_TinhHinhMuonSachBUS instance;
+        private CT_BC_TinhHinhMuonSachBUS() { }
+
+        public static CT_BC_TinhHinhMuonSachBUS Instance
+        {
+            get
+            {
+                if (instance == null) instance = new CT_BC_TinhHinhMuonSachBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllCT_BCTinhHinhMuonSachByIDBC(int ID)
+        {
+            return CT_BC_TinhHinhMuonSachDAL.Instance.GetAllCT_BCTinhHinhMuonSachByIDBC(ID);
+        }
+        public int InsertCT_BCTinhHinhMuonSach(CT_BC_TinhHinhMuonSach CTBC)
+        {
+            return CT_BC_TinhHinhMuonSachDAL.Instance.InsertCT_BCTinhHinhMuonSach(CTBC);
+        }
+    }
+}
---
+++ DAL/CT_BC_TinhHinhMuonSachDAL.cs
@@ -0,0 +1,38 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CT_BC_TinhHinhMuonSachDAL
+    {
+        private static CT_BC_TinhHinhMuonSachDAL instance;
+        private CT_BC_TinhHinhMuonSachDAL() { }
+        public static CT_BC_TinhHinhMuonSachDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new CT_BC_TinhHinhMuonSachDAL();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllCT_BCTinhHinhMuonSachByIDBC(int ID)
+        {
+            string query = "SELECT TENTHELOAI, SOLUOTMUON, TILE " +
+                           "FROM BC_TINHHINHMUONSACH JOIN CT_BC_TINHHINHMUONSACH ON BC_TINHHINHMUONSACH.ID = CT_BC_TINHHINHMUONSACH.IDBCTHMS " +
+                           "JOIN THELOAI ON THELOAI.ID = CT_BC_TINHHINHMUONSACH.IDTHELOAI " +
+                           "WHERE BC_TINHHINHMUONSACH.ID = '" + ID + "'";
+            return DataProvider.Instance.ExecuteQuery(query);
+        }
+        public int InsertCT_BCTinhHinhMuonSach(CT_BC_TinhHinhMuonSach CTBC)
+        {
+            string query = "INSERT INTO CT_BC_TINHHINHMUONSACH VALUES('" + CTBC.IDBCTHMS + "', '" + CTBC.IDTheLoai + "', '" + CTBC.SoLuotMuon + "', '" + CTBC.TiLe + "')";
+            return DataProvider.Instance.ExecuteNonQuery(query);
+        }
+    }
+}
