90e3e4e871b8bd54f6f5e13ce90ce6a602caa394
21522631 <106432240+21522631@users.noreply.github.com>
2023-12-20 11:32:59 +0700

    Thêm BUS nhóm người dùng

---
+++ BUS/NhomNguoiDungBUS.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class NhomNguoiDungBUS
+    {
+        private static NhomNguoiDungBUS instance;
+        private NhomNguoiDungBUS() { }
+
+        public static NhomNguoiDungBUS Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new NhomNguoiDungBUS();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable GetAllNhomNguoiDung()
+        {
+            return NhomNguoiDungDAL.Instance.GetAllNhomNguoiDung();
+        }
+    }
+}
